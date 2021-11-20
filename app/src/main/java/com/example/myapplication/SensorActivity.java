cb3fae411d74f7123960081d5dac6b1c4fc4c3a3
Zhengyang Song <songzy_thu@163.com>
2021-11-21 00:58:54 +0800

    Use the rotation example.

--- app/src/main/java/com/example/myapplication/SensorActivity.java
+++ app/src/main/java/com/example/myapplication/SensorActivity.java
@@ -1,108 +1,189 @@
-/*
- * Copyright (C) 2007 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.myapplication;
 
 import android.app.Activity;
-import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
-import android.util.Log;
 
-public class SensorActivity extends Activity implements SensorEventListener {
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 
-    private static final String TAG = "MyActivity";
-    private final float[] accelerometerReading = new float[3];
-    private final float[] magnetometerReading = new float[3];
-    private final float[] rotationMatrix = new float[9];
-    private final float[] orientationAngles = new float[3];
-    private SensorManager sensorManager;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Wrapper activity demonstrating the use of the new
+ * {@link SensorEvent#values rotation vector sensor}
+ * ({@link Sensor#TYPE_ROTATION_VECTOR TYPE_ROTATION_VECTOR}).
+ *
+ * @see Sensor
+ * @see SensorEvent
+ * @see SensorManager
+ */
+public class SensorActivity extends Activity {
+    private GLSurfaceView mGLSurfaceView;
+    private SensorManager mSensorManager;
+    private MyRenderer mRenderer;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Do something here if sensor accuracy changes.
-        // You must implement this callback in your code.
+        // Get an instance of the SensorManager
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        // Create our Preview view and set it as the content of our
+        // Activity
+        mRenderer = new MyRenderer();
+        mGLSurfaceView = new GLSurfaceView(this);
+        mGLSurfaceView.setRenderer(mRenderer);
+        setContentView(mGLSurfaceView);
     }
 
     @Override
     protected void onResume() {
+        // Ideally a game should implement onResume() and onPause()
+        // to take appropriate action when the activity looses focus
         super.onResume();
-
-        // Get updates from the accelerometer and magnetometer at a constant rate.
-        // To make batch operations more efficient and reduce power consumption,
-        // provide support for delaying updates to the application.
-        //
-        // In this example, the sensor reporting delay is small enough such that
-        // the application receives an update before the system checks the sensor
-        // readings again.
-        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        if (accelerometer != null) {
-            sensorManager.registerListener(this, accelerometer,
-                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
-        }
-        Sensor magneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        if (magneticField != null) {
-            sensorManager.registerListener(this, magneticField,
-                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
-        }
+        mRenderer.start();
+        mGLSurfaceView.onResume();
     }
 
     @Override
     protected void onPause() {
+        // Ideally a game should implement onResume() and onPause()
+        // to take appropriate action when the activity looses focus
         super.onPause();
-
-        // Don't receive any more updates from either sensor.
-        sensorManager.unregisterListener(this);
+        mRenderer.stop();
+        mGLSurfaceView.onPause();
     }
 
-    // Get readings from accelerometer and magnetometer. To simplify calculations,
-    // consider storing these readings as unit vectors.
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            System.arraycopy(event.values, 0, accelerometerReading,
-                    0, accelerometerReading.length);
-        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-            System.arraycopy(event.values, 0, magnetometerReading,
-                    0, magnetometerReading.length);
+    class MyRenderer implements GLSurfaceView.Renderer, SensorEventListener {
+        private final float[] mRotationMatrix = new float[16];
+        private final Cube mCube;
+        private final Sensor mRotationVectorSensor;
+
+        public MyRenderer() {
+            // find the rotation-vector sensor
+            mRotationVectorSensor = mSensorManager.getDefaultSensor(
+                    Sensor.TYPE_ROTATION_VECTOR);
+            mCube = new Cube();
+            // initialize the rotation matrix to identity
+            mRotationMatrix[0] = 1;
+            mRotationMatrix[4] = 1;
+            mRotationMatrix[8] = 1;
+            mRotationMatrix[12] = 1;
         }
-        updateOrientationAngles();
-        Log.i(TAG, String.valueOf(orientationAngles));
-    }
 
-    // Compute the three orientation angles based on the most recent readings from
-    // the device's accelerometer and magnetometer.
-    public void updateOrientationAngles() {
-        // Update rotation matrix, which is needed to update orientation angles.
-        SensorManager.getRotationMatrix(rotationMatrix, null,
-                accelerometerReading, magnetometerReading);
+        public void start() {
+            // enable our sensor when the activity is resumed, ask for
+            // 10 ms updates.
+            mSensorManager.registerListener(this, mRotationVectorSensor, 10000);
+        }
 
-        // "rotationMatrix" now has up-to-date information.
+        public void stop() {
+            // make sure to turn our sensor off when the activity is paused
+            mSensorManager.unregisterListener(this);
+        }
 
-        SensorManager.getOrientation(rotationMatrix, orientationAngles);
+        public void onSensorChanged(SensorEvent event) {
+            // we received a sensor event. it is a good practice to check
+            // that we received the proper event
+            if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+                // convert the rotation-vector to a 4x4 matrix. the matrix
+                // is interpreted by Open GL as the inverse of the
+                // rotation-vector, which is what we want.
+                SensorManager.getRotationMatrixFromVector(
+                        mRotationMatrix, event.values);
+            }
+        }
 
-        // "orientationAngles" now has up-to-date information.
+        public void onDrawFrame(GL10 gl) {
+            // clear screen
+            gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+            // set-up modelview matrix
+            gl.glMatrixMode(GL10.GL_MODELVIEW);
+            gl.glLoadIdentity();
+            gl.glTranslatef(0, 0, -3.0f);
+            gl.glMultMatrixf(mRotationMatrix, 0);
+            // draw our object
+            gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+            gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+            mCube.draw(gl);
+        }
+
+        public void onSurfaceChanged(GL10 gl, int width, int height) {
+            // set view-port
+            gl.glViewport(0, 0, width, height);
+            // set projection matrix
+            float ratio = (float) width / height;
+            gl.glMatrixMode(GL10.GL_PROJECTION);
+            gl.glLoadIdentity();
+            gl.glFrustumf(-ratio, ratio, -1, 1, 1, 10);
+        }
+
+        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+            // dither is enabled by default, we don't need it
+            gl.glDisable(GL10.GL_DITHER);
+            // clear screen in white
+            gl.glClearColor(1, 1, 1, 1);
+        }
+
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        class Cube {
+            // initialize our cube
+            private final FloatBuffer mVertexBuffer;
+            private final FloatBuffer mColorBuffer;
+            private final ByteBuffer mIndexBuffer;
+
+            public Cube() {
+                final float[] vertices = {
+                        -1, -1, -1, 1, -1, -1,
+                        1, 1, -1, -1, 1, -1,
+                        -1, -1, 1, 1, -1, 1,
+                        1, 1, 1, -1, 1, 1,
+                };
+                final float[] colors = {
+                        0, 0, 0, 1, 1, 0, 0, 1,
+                        1, 1, 0, 1, 0, 1, 0, 1,
+                        0, 0, 1, 1, 1, 0, 1, 1,
+                        1, 1, 1, 1, 0, 1, 1, 1,
+                };
+                final byte[] indices = {
+                        0, 4, 5, 0, 5, 1,
+                        1, 5, 6, 1, 6, 2,
+                        2, 6, 7, 2, 7, 3,
+                        3, 7, 4, 3, 4, 0,
+                        4, 7, 6, 4, 6, 5,
+                        3, 0, 1, 3, 1, 2
+                };
+                ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.length * 4);
+                vbb.order(ByteOrder.nativeOrder());
+                mVertexBuffer = vbb.asFloatBuffer();
+                mVertexBuffer.put(vertices);
+                mVertexBuffer.position(0);
+                ByteBuffer cbb = ByteBuffer.allocateDirect(colors.length * 4);
+                cbb.order(ByteOrder.nativeOrder());
+                mColorBuffer = cbb.asFloatBuffer();
+                mColorBuffer.put(colors);
+                mColorBuffer.position(0);
+                mIndexBuffer = ByteBuffer.allocateDirect(indices.length);
+                mIndexBuffer.put(indices);
+                mIndexBuffer.position(0);
+            }
+
+            public void draw(GL10 gl) {
+                gl.glEnable(GL10.GL_CULL_FACE);
+                gl.glFrontFace(GL10.GL_CW);
+                gl.glShadeModel(GL10.GL_SMOOTH);
+                gl.glVertexPointer(3, GL10.GL_FLOAT, 0, mVertexBuffer);
+                gl.glColorPointer(4, GL10.GL_FLOAT, 0, mColorBuffer);
+                gl.glDrawElements(GL10.GL_TRIANGLES, 36, GL10.GL_UNSIGNED_BYTE, mIndexBuffer);
+            }
+        }
     }
 }
